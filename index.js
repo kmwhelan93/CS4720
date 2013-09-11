d7d4c3ffd9d1fe2433a4a81e9286d0e2ccb48d1c
Kevin Whelan <km.whelan93@gmail.com>
2013-09-11 02:53:59 -0400

    added picture and email stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 var HEIGHT = 250, 
-	WIDTH = 250;
+	WIDTH = 250; // on change, need to propagate to index.css
 
 var stage = new Kinetic.Stage({
 	container: 'pro-pic',
@@ -31,10 +31,27 @@ proPic.onload = function() {
 	var redLineX = 0;
 	var redLineY = 0;
 	var down = false;
+	var lines = [];
+	var lineIndex = -1;
 
 	layer.add(me);
 
 	layer.add(redLine);
+
+	var anim = new Kinetic.Animation(function(frame) {
+		var time = frame.time,
+			timeDiff = frame.timeDiff,
+			frameRate = frame.frameRate;
+		if (down) {
+			var mouse = stage.getMousePosition();
+			if (mouse) {
+				var points = redLine.getPoints();
+				points.push({x:mouse.x, y:mouse.y});
+				redLine.setPoints(points);
+			}
+		}
+	}, layer);
+
 
 	redLine.setPoints([0, 0, 0, 0]);
 	document.onmousedown = function() {
@@ -44,13 +61,28 @@ proPic.onload = function() {
 			redLineY = mouse.y;
 			down = true;
 			redLine = new Kinetic.Line({
-				points: [0, 0, 0, 0],
+				points: [redLineX, redLineY],
 				stroke: "red",
 				strokeWidth: 5,
 				lineCap: "round",
 				lineJoin: "round"
 			});
 			layer.add(redLine);
+			lines.push(redLine);
+
+			//might be invisible ones right now
+			lineIndex = lines.length - 1;
+
+			//remove all beyond this index
+			for (var x = 0; x < lines.length; x++) {
+				if (lines[x].cache.visible == false) {
+					lines.splice(x, 1);
+					x--;
+					// because x will always be less than lineIndex
+					lineIndex--;
+				}
+			}
+			anim.start();
 		}
 	};
 
@@ -58,22 +90,68 @@ proPic.onload = function() {
 		var mouse = stage.getMousePosition();
 		if (mouse) {
 			down = false;
+			anim.stop();
 		}
 	}
 
-	var anim = new Kinetic.Animation(function(frame) {
-		var time = frame.time,
-			timeDiff = frame.timeDiff,
-			frameRate = frame.frameRate;
-		if (down) {
-			var mouse = stage.getMousePosition();
-			if (mouse) {
-				redLine.setPoints([redLineX, redLineY, mouse.x, mouse.y]);
+	function undo() {
+		if (lineIndex > -1 && lineIndex < lines.length) {
+			var line = lines[lineIndex];
+			line.hide();
+			if (lineIndex > 0) {
+				lineIndex--;
 			}
+			layer.draw();
 		}
-	}, layer);
+	}
 
-	anim.start();
+	function redo() {
+		if (lineIndex > -1 && lineIndex < lines.length) {
+			var line = lines[lineIndex];
+			line.show();
+			if (lineIndex < lines.length - 1) {
+				lineIndex++;
+			}
+			layer.draw();
+		}
+	}
+
+	$(document).ready(function() {
+		$("#undo-button").on("click", function () {
+			undo();
+		});
+
+		$("#redo-button").on("click", function () {
+			redo();
+		});
+
+		$("#send-button").on("click", function () {
+
+			stage.toDataURL({
+	        	callback: function(dataUrl) {
+	            /*
+	             * here you can do anything you like with the data url.
+	             * In this tutorial we'll just open the url with the browser
+	             * so that you can see the result as an image
+	             */
+	        		$.ajax({
+	        			type: "POST",
+						url: "sendPictureEmail.php",
+						data: {dataURL: dataUrl, from: "me"}, //todo fix
+						success: function (data) {
+							alert("success");
+						},
+						error: function() {
+							alert("fail!");
+						}
+					});
+	        	}
+	     	});
+			
+		});
+	});
+
+
 
 
     stage.add(layer);
---
+++ sendPictureEmail.php
@@ -0,0 +1,19 @@
+<?php
+	$to = "km.whelan93@gmail.com";
+	$subject = "Someone drew a photo of you!";
+	$from = "km.whelan93@gmail.com";
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= "From:" . $from . "\r\n";
+	mail($to, $subject, "hi", $headers);
+	$theMessage="<html>
+	<body>
+		" . $_GET["dataURL"] . "
+	</body>
+	</html>
+	";
+	mail($to,$subject,$theMessage,$headers);
+	echo "hi";
+
+
+?>

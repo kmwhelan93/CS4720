254f1ed6b79e1654ab12665b26f60e51d8170831
Kevin Whelan <km.whelan93@gmail.com>
2013-09-11 04:06:39 -0400

    minor change

--- sendPictureEmail.php
+++ sendPictureEmail.php
@@ -6,20 +6,22 @@
 
 	$body             = "hi";
 
-	$mail->AddReplyTo("name@yourdomain.com","First Last");
+	// $mail->AddReplyTo("name@yourdomain.com","First Last");
 
-	$mail->SetFrom('name@yourdomain.com', 'First Last');
+	$mail->SetFrom('hompage', 'my homepage');
 
-	$mail->AddReplyTo("name@yourdomain.com","First Last");
+	// $mail->AddReplyTo("name@yourdomain.com","First Last");
 
 	$address = "km.whelan93@gmail.com";
-	$mail->AddAddress($address, "John Doe");
+	$mail->AddAddress($address, "Kevin Whelan");
 
-	$mail->Subject    = "PHPMailer Test Subject via mail(), basic";
+	$mail->Subject    = $_POST['from'] . " sent you a message from your homepage!";
 
 	$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
 
 	$mail->MsgHTML($body);
+
+	$mail->AddStringAttachment($_POST['dataURL'], 'propic', "base64", ".png");
 
 	// $mail->AddAttachment("images/phpmailer.gif");      // attachment
 	// $mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
@@ -29,38 +31,5 @@
 	} else {
 	  echo "Message sent!";
 	}
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	$to = "km.whelan93@gmail.com";
-	$subject = "Someone drew a photo of you!";
-	$from = "km.whelan93@gmail.com";
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	$headers .= "From:" . $_POST["from"] . "\r\n";
-	$theMessage="<html>
-	<body>
-		" . $_POST["dataURL"] . "
-	</body>
-	</html>
-	";
-	mail($to,$subject,$theMessage,$headers);
-	echo "hi";
-	echo $_POST['dataURL'];
-
 
 ?>

c2475b9838d59a5bdf0eb06cefc14e9da96072d1
Kevin Whelan <km.whelan93@gmail.com>
2013-09-11 03:52:50 -0400

    test PHP Mailer

--- sendPictureEmail.php
+++ sendPictureEmail.php
@@ -1,4 +1,51 @@
 <?php
+
+	require_once('class.phpmailer.php');
+
+	$mail             = new PHPMailer(); // defaults to using php "mail()"
+
+	$body             = "hi";
+
+	$mail->AddReplyTo("name@yourdomain.com","First Last");
+
+	$mail->SetFrom('name@yourdomain.com', 'First Last');
+
+	$mail->AddReplyTo("name@yourdomain.com","First Last");
+
+	$address = "km.whelan93@gmail.com";
+	$mail->AddAddress($address, "John Doe");
+
+	$mail->Subject    = "PHPMailer Test Subject via mail(), basic";
+
+	$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+
+	$mail->MsgHTML($body);
+
+	// $mail->AddAttachment("images/phpmailer.gif");      // attachment
+	// $mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
+
+	if(!$mail->Send()) {
+	  echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+	  echo "Message sent!";
+	}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	$to = "km.whelan93@gmail.com";
 	$subject = "Someone drew a photo of you!";
 	$from = "km.whelan93@gmail.com";
@@ -7,7 +54,7 @@
 	$headers .= "From:" . $_POST["from"] . "\r\n";
 	$theMessage="<html>
 	<body>
-		<a href=\"" . $_POST["dataURL"] . "\">the photo</a>
+		" . $_POST["dataURL"] . "
 	</body>
 	</html>
 	";

95955f0472adad35c81734507a44e496b8e4a37e
Kevin Whelan <km.whelan93@gmail.com>
2013-09-11 03:22:57 -0400

    fixed email

--- sendPictureEmail.php
+++ sendPictureEmail.php
@@ -4,15 +4,16 @@
 	$from = "km.whelan93@gmail.com";
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	$headers .= "From:" . $_GET["from"] . "\r\n";
+	$headers .= "From:" . $_POST["from"] . "\r\n";
 	$theMessage="<html>
 	<body>
-		" . $_GET["dataURL"] . "
+		" . $_POST["dataURL"] . "
 	</body>
 	</html>
 	";
 	mail($to,$subject,$theMessage,$headers);
 	echo "hi";
+	echo $_POST['dataURL'];
 
 
 ?>

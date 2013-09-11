7656ab39bd1516bd7503c289506da2ea0fb2984d
Kevin Whelan <km.whelan93@gmail.com>
2013-09-11 02:58:00 -0400

    minor change

--- sendPictureEmail.php
+++ sendPictureEmail.php
@@ -4,8 +4,7 @@
 	$from = "km.whelan93@gmail.com";
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	$headers .= "From:" . $from . "\r\n";
-	mail($to, $subject, "hi", $headers);
+	$headers .= "From:" . $_GET["from"] . "\r\n";
 	$theMessage="<html>
 	<body>
 		" . $_GET["dataURL"] . "
